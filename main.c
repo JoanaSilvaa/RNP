97c5fbfd4988313a914d05a89fc0cd845f4c18f6
juoola <104073941+juoola@users.noreply.github.com>
2023-10-24 18:02:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <libprg/libprg.h>
 
 int main(int argc, char *argv[]) {
-    //printf("Entre com um numero");
+    //mensagem que printa junto comos resultados recebidos da função
     printf("Conforme as informacoes passadas via linha de comando o resultado eh: ");
+    //função que contem o resultado da calculadora
+    //recebe como argumento o argc que contem o numero de argumentos recebidos via linha de comando
+    //e recebe o argv que são os operadores e operandos para serem usadas na calculadora
     Notacao(argc,argv);
     return 0;
 }
